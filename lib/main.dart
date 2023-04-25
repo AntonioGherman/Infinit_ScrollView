b7c79a6ec5c7cacb8bec40a44556ac2971b0f654
AntonioGherman <antoniogherman15@gmail.com>
2023-04-25 20:41:54 +0300

    adaugarea unei constante

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Random Photo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
