9271525307d655f2c545767d70d5e0a3955db587
AntonioGherman <antoniogherman15@gmail.com>
2023-04-25 20:39:37 +0300

    Imbunatatirea codului

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'favorite_images_page.dart';
 import 'new_photo_page.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -12,24 +13,24 @@ class HomePage extends StatefulWidget {
 int _currentIndex = 0;
 
 class _HomePageState extends State<HomePage> {
-  List<Widget> page = <Widget>[NewPhotoPage(), FavoritePhotosPage()];
+  List<Widget> page = <Widget>[const NewPhotoPage(), const FavoritePhotosPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: _currentIndex == 0 ? Text('Random Photo') : Text('Favorite Photos'),
+          title: _currentIndex == 0 ? const Text('Random Photo') : const Text('Favorite Photos'),
           centerTitle: true,
         ),
         body: page[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
-          onTap: (index) => setState(() {
+          onTap: (int index) => setState(() {
             _currentIndex = index;
           }),
           type: BottomNavigationBarType.fixed,
           selectedItemColor: Colors.grey,
-          items: [
+          items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.photo, color: _currentIndex == 0 ? Colors.blue : null),
               label: 'New Photo',
